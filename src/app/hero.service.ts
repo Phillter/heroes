12895876a82498c232151d5a5fe34e5c940e5a8d
Phillter <philltrev@gmail.com>
2018-07-26 14:39:40 +0900

    Added messages component and added support for it to heroesService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,15 +2,18 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessagesService } from './messages.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor( private messagesService: MessagesService) { }
 
   getHeroes(): Observable<Hero[]> {
+    // TODO: Send message after fetching heroes from remote server
+    this.messagesService.add('HeroService: fetched Heroes list')
     return of(HEROES);
   }
 }
