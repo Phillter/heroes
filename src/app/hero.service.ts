08f21a85f6f1f0cbc96b3a12ba6fd3289d032bbb
Phillter <philltrev@gmail.com>
2018-07-27 09:41:08 +0900

    Shifting to use HTTP calls to get data. currently using internal memory mock server.
    Note: Error about recognizing in-memory-data.service.ts and finding mock-heroes.ts

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,25 +1,56 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessagesService } from './messages.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class HeroService {
 
-  constructor( private messagesService: MessagesService) { }
+export class HeroService {
+  private heroesUrl = 'api/heroes'; //URL to the web api
+
+  constructor(
+    private messagesService: MessagesService,
+    private http: HttpClient ) { }
+
+  private log(message: string) {
+    this.messagesService.add('HeroService: ${message}');
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      //TODO: send error to remote logging infrastructure
+      console.error(error); //log error to console
+
+      //TODO: make error message more clear for user
+      this.log('${operation} failed: ${error.message}');
+
+      //allow app to continue by returning empty result
+      return of(result as T);
+    }
+  }
 
   getHeroes(): Observable<Hero[]> {
     // TODO: Send message after fetching heroes from remote server
     this.messagesService.add('HeroService: fetched Heroes list')
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
+              .pipe(
+                tap(heroes => this.log('fetched heroes')),
+                catchError(this.handleError('getHeroes', []))
+              );
   }
 
   getHero(id: number): Observable<Hero> {
     //TODO: Send message after getting hero specified
-    this.messagesService.add('HeroService: fetched hero id=${id}');
-    return of(HEROES.find(hero => hero.id === id));
+    const url = '${this.heroesUrl}/${id}';
+    return this.http.get<Hero>(url)
+      .pipe(
+        tap(_ => this.log('fetched hero id=${id}')),
+        catchError(this.handleError<Hero>('getHero id=${id}'))
+      );
   }
 }
